162cb7cd4cd22ef41dc99fb5fc90db1920ee5b91
lqs2002 <749062870@qq.com>
2022-07-01 14:24:08 +0800

    springcloudtenet学习

---
+++ springcloudtencent/src/main/java/com/example/springcloudtencent/SpringcloudtencentApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcloudtencent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudtencentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudtencentApplication.class, args);
+    }
+
+}
---
+++ springcloudtencent/src/test/java/com/example/springcloudtencent/SpringcloudtencentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloudtencent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudtencentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
